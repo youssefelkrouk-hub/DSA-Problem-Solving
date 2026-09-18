c71eef34fb8d183805d9abf105313690b7243a20
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-19 00:26:26 +0100

    Other Brute Froce Solution

--- Next _Greater _Element _I.py
+++ Next _Greater _Element _I.py
@@ -19,3 +19,20 @@ n2=[1,3,4,2]
 print(nextGreaterElement(n1,n2))
 
 # this a brute force a  proach , where time complexity if O(n*msquare) and space complexity is O(n)
+
+# antoher brute force aproach using less more extra  memory : 
+print("\n")
+def nextgreatelt(nums1, nums2):
+    result = [-1]*(len(nums1))
+    for index,x in enumerate(nums1):
+        pos = nums2.index(x)
+        for k in range(pos + 1, len(nums2)):
+            if nums2[k] > x:
+                result[index]=nums2[k]
+                break
+    return result
+
+nums1=[4,1,2]
+nums2=[1,3,4,2]
+print(nextgreatelt(nums1,nums2))
+
