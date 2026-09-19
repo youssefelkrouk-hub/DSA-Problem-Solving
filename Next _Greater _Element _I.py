3f037c98ed21822990adeb11a031a2ac18ef450b
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-19 01:45:44 +0100

    Monotonic Stack implementation for nge

--- Next _Greater _Element _I.py
+++ Next _Greater _Element _I.py
@@ -1,3 +1,33 @@
+# let's implement the next greater element in a array , using a monotonic stack : 
+
+
+def nge(nums):
+    n = len(nums)
+    res = [0] * n
+    stack = []
+    for i in range(n - 1, -1, -1):
+        while stack and stack[-1] <= nums[i]:
+            stack.pop()
+        if stack:
+            res[i] = stack[-1]   # top of stack = next greater element
+        else:
+            res[i] = -1          # nothing greater found
+        stack.append(nums[i])    # push current element for future comparisons
+    return res
+
+print(nge([4, 5, 2, 25]))
+
+
+# The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+# You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+# For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+# Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+
+
 def nextGreaterElement(nums1, nums2):
     res=[-1]*(len(nums1))
     hash_map={}
